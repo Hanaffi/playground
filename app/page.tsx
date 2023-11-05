2358d38dff4ac4464aaffd71e4ae4d828e72417c
Hanaffi <ahmedellord56@gmail.com>
2023-11-05 19:46:55 +0200

    add styleguide colors and base input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
+import Image from 'next/image'
+import NumInputWithIcon from './components/NumInputWithIcon'
 import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      TASKKKK
+      <NumInputWithIcon
+        min="0"
+        label="Price" Icon={<Image src="/icon-dollar.svg" alt="dollar" width={12} height={12} />} />
     </main>
   )
 }
