34c4911c70bd260910de2870ad0631646c8795df
Hanaffi <ahmedellord56@gmail.com>
2023-11-06 11:48:36 +0200

    add calculation and some stylings

--- app/components/PricingCard/index.tsx
+++ app/components/PricingCard/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Input from '../Input'
 import styles from './PricingCard.module.scss'
-import { useState } from 'react'
+import {  useState } from 'react'
 import Checkbox from '../Checkbox'
 
 const STANDARD_TIPS = [5, 10, 15, 25, 50];
@@ -11,10 +11,35 @@ type Tip = {
     type: "custom" | "standard"
 }
 
+
+type PricingRowProps = {
+    label: string
+    value: number
+}
+
+function PricingRow({
+    label,
+    value
+}: PricingRowProps) {
+    const prettyValue = value.toFixed(2)
+
+    return <div className={styles.row}>
+        <div className={styles.pricingRowInfoWrapper}>
+            <div className={styles.pricingRowLabel}>{label}</div>
+            <div className={styles.pricingRowPerPerson}>/ person</div>
+        </div>
+        <div className={styles.pricingRowValue}>${prettyValue}</div>
+    </div>
+}
+
+
 function PricingCard() {
     const [bill, setBill] = useState(0)
     const [people, setPeople] = useState(0)
     const [tip, setTip] = useState<Tip | null>(null)
+
+    const tipAmountPerPerson = people == 0 ? 0 : bill * ((tip?.value ?? 0) / 100) / people
+    const totalAmountPerPerson = people == 0 ? 0 : bill / people + tipAmountPerPerson
 
     const handleBillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setBill(Number(e.target.value))
@@ -23,8 +48,28 @@ function PricingCard() {
         setPeople(Math.floor(Number(e.target.value)))
     }
 
+    const handleCustomBillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTip({
+            value: Number(e.target.value),
+            type: "custom"
+        })
+    }
+
+    const handleStandardBillClick = (standardTip: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTip(_prev => (event.target as HTMLInputElement).checked ? ({
+            value: standardTip,
+            type: "standard"
+        }) : null)
+    }
+
+    const handleReset = (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setBill(0)
+        setPeople(0)
+        setTip(null)
+    }
+
     return <div className={styles.card}>
-        <div className="inputsWrapper">
+        <div className={styles.inputsWrapper}>
             <Input
                 min="0"
                 label="Bill"
@@ -32,42 +77,42 @@ function PricingCard() {
                 onChange={handleBillChange}
                 Icon={<Image src="icon-dollar.svg" alt="dollar" width={12} height={12} />}
             />
+
+            <div className={styles.tipsWrapper}>
+                {STANDARD_TIPS.map(standardTip => {
+                        const checked = tip?.type == "standard" && tip?.value == standardTip
+
+                        return <Checkbox key={standardTip} label={`${standardTip}%`}
+                            checked={checked}
+                            onChange={handleStandardBillClick(standardTip)}
+                        />
+                    })}
+                <Input
+                    min="0"
+                    value={tip?.type == "custom" ? tip.value : ""}
+                    onChange={handleCustomBillChange}
+                    placeholder='Custom'
+                    className={styles.customInput}
+                />
+            </div>
+
             <Input
                 min="0"
                 step={1}
-                label="Number of People"
                 value={people}
+                label="Number of People"
                 onChange={handlePeopleChange}
                 Icon={<Image src="icon-person.svg" alt="dollar" width={12} height={12} />}
             />
+        </div>
 
-            <div className={styles.tipsWrapper}>
-            {
-                STANDARD_TIPS.map(standardTip => {
-                    const checked = tip?.type == "standard" && tip?.value == standardTip
-
-                    return <Checkbox key={standardTip} label={`${standardTip}%`}
-                        checked={checked}
-                        onChange={(event) => {
-                            setTip(prev=>(event.target as HTMLInputElement).checked ? ({
-                                value: standardTip,
-                                type: "standard"
-                            }) : null)
-                        }}
-
-                    />
-                })
-            }
-
-            <Input min="0" value={tip?.type == "custom" ? tip.value : ""} onChange={event => {
-                setTip({
-                    value: Number((event.target as HTMLInputElement).value),
-                    type: "custom"
-                })
-            }}
-                placeholder='Custom'
-            />
+        <div className={styles.resultWrapper}>
+            <div className={styles.pricingRowsWrapper}>
+                <PricingRow value={tipAmountPerPerson} label='Tip Amount' />
+                <PricingRow value={totalAmountPerPerson} label='Total' />
             </div>
+
+            <button className={styles.resetBtn} onClick={handleReset}>Reset</button>
         </div>
     </div>
 }
